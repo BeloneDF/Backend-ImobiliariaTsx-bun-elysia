a6f79c314030ec082e3e26e6f88aaf662b843ab1
Belone Zorzetto Fraga <40274926+BeloneDF@users.noreply.github.com>
2024-02-14 14:37:26 -0300

    crud imoveis

--- src/controllers/imovel.ts
+++ src/controllers/imovel.ts
@@ -1,11 +1,13 @@
-import { Elysia } from "elysia";
+import { Elysia, t } from "elysia";
 import * as Repo from "../repositories/imovel.repo";
 import { Imovel } from "../types/imovel/imovel.types";
 
 export const imovelController = (app: Elysia) => {
-  app
-    .post("/imovel", ({ body }) => Repo.AddImovel(body as Imovel))
-    .onAfterHandle(() => {
-      "Imóvel criado com sucesso";
-    });
+  app.post("/imovel", ({ body }) => Repo.AddImovel(body as Imovel));
+  app.get("/imovel", () => Repo.FindAllImovel());
+  app.get("/imovel/:id", ({ params }) => Repo.GetImovelById(params.id));
+  app.delete("/imovel/:id", ({ params, body }) => Repo.DeleteImovel(params.id));
+  app.put("/imovel/:id", ({ params, body }) =>
+    Repo.UpdateImovel(params.id, body as Imovel)
+  );
 };
--- src/repositories/imovel.repo.ts
+++ src/repositories/imovel.repo.ts
@@ -19,7 +19,61 @@ export async function AddImovel(data: Imovel) {
         //data: data,
       };
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
+  }
+}
+
+export async function FindAllImovel() {
+  try {
+    const data = await prisma.imovel.findMany();
+    return {
+      code: 200,
+      data: data,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function GetImovelById(params: string) {
+  try {
+    const data = await prisma.imovel.findUnique({ where: { id: params } });
+    return {
+      message:
+        data === null
+          ? "Item encontrado, mas sem dados"
+          : "Imovel encontrado com sucesso!",
+      code: 202,
+      data: data,
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function DeleteImovel(params: string) {
+  try {
+    await prisma.imovel.delete({ where: { id: params } });
+    return {
+      message: "Imóvel deletado com sucesso!",
+    };
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function UpdateImovel(params: string, data: Imovel) {
+  try {
+    await prisma.imovel.update({
+      where: { id: params },
+      data: data,
+    });
+    return {
+      message: "Imovel alterado com sucesso!",
+      data: data,
+    };
+  } catch (error) {
+    console.log(error);
   }
 }
